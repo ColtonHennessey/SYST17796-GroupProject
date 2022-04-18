bafce8c95bc27409c7f4d1aa11eb90172efb4ee2
Colton Hennessey <hennecol@sheridancollege.ca>
2022-04-17 22:03:30 -0400

    Added the removePlayer method.

--- src/ca/sheridancollege/project/Blackjack.java
+++ src/ca/sheridancollege/project/Blackjack.java
@@ -19,7 +19,7 @@ import java.util.Scanner;
 public class Blackjack
 {
 
-   private ArrayList<Player> players = new ArrayList<>();
+   private final ArrayList<Player> players = new ArrayList<>();
    private int roundNumber;
    private int playerCount;
    Scanner scan = new Scanner(System.in);
@@ -27,23 +27,47 @@ public class Blackjack
    public void playGame ()
    {
       addPlayer();
+      removePlayer();
    }
 
    public void addPlayer ()
    {
-      System.out.println("Do you want to add a player? \n ENTER YES OR NO");
+      System.out.println("Do you want to add a player? \n ENTER YES OR NO \n");
       String playerInput = scan.next();
       while (playerInput.equalsIgnoreCase("YES")) {
-         System.out.println("Enter Player Name: ");
+         System.out.println("Enter Player Name:  \n");
          String inputName = scan.next();
          players.add(new Player(inputName, this.playerCount));
-         System.out.println("Adding " + players.get(this.playerCount).getPlayerName() + " to the game");
+         System.out.println("Added " + players.get(this.playerCount).getPlayerName() + " to the game \n");
          this.playerCount++;
-         System.out.println("Did you want to add another player? \n YES / NO");
+         System.out.println("Did you want to add another player? \n YES / NO \n");
          playerInput = scan.next();
       }
+      System.out.println("List of Players: \n");
       for (int i = 0; i < players.size(); i++) {
-         System.out.println(" # " + players.get(i).getPlayerNumber() + " : " + players.get(i).getPlayerName());
+         System.out.println(" # " + players.get(i).getPlayerNumber() + " : " + players.get(i).getPlayerName() + "\n");
+      }
+   }
+
+   public void removePlayer ()
+   {
+      System.out.println("Do you want to remove a player? \n ENTER YES OR NO \n ");
+      String playerInput = scan.next();
+      while (playerInput.equalsIgnoreCase("YES")) {
+         System.out.println("Enter Player Name \n ");
+         String inputName = scan.next();
+         for (int i = 0; i < players.size(); i++) {
+            if (inputName.equalsIgnoreCase(players.get(i).getPlayerName())) {
+               System.out.println("Removed " + players.get(i).getPlayerName() + " from the game \n");
+               players.remove(i);
+            }
+         }
+         System.out.println("Do you want to remove another player? \n ENTER YES OR NO \n");
+         playerInput = scan.next();
+      }
+      System.out.println("List of Players: \n");
+      for (int i = 0; i < players.size(); i++) {
+         System.out.println("# " + players.get(i).getPlayerNumber() + " : " + players.get(i).getPlayerName() + "\n");
       }
    }
 
